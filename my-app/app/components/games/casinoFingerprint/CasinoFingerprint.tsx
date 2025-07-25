49de1e5ce70133ee92ba11cb357994ac7d9db07f
AnuragVatash <109451690+AnuragVatash@users.noreply.github.com>
2025-07-25 15:29:22 -0700

    Casino Fingerprint Minigame Init

---
+++ my-app/app/components/games/casinoFingerprint/CasinoFingerprint.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+
+export default function CasinoFingerprint() {
+  return (
+    <div>
+      <h1>Casino Fingerprint Game</h1>
+    <div>
+        <img src="/casinoFingerprints/casino1print1.jpg"/>
+        <img src="/casinoFingerprints/casino1print2.jpg"/>
+        <img src="/casinoFingerprints/casino1print3.jpg"/>
+        <img src="/casinoFingerprints/casino1print4.jpg"/>
+        <img src="/casinoFingerprints/casino1print5.jpg"/>
+        <img src="/casinoFingerprints/casino1print6.jpg"/>
+        <img src="/casinoFingerprints/casino1print7.jpg"/>
+        <img src="/casinoFingerprints/casino1print8.jpg"/>
+        </div>
+    </div>
+  );
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,11 @@
-import CasinoFingerprint from '@/app/components/games/casinoFingerprint/CasinoFingerprint';
+import CasinoFingerprint from './components/games/casinoFingerprint/CasinoFingerprint';
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome</h1>
         <p className="text-lg text-gray-600">Your Next.js app is ready!</p>
+        <CasinoFingerprint />
       </div>
     </div>
   );
