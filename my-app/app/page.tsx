e3a78e93d90d03cb9fd06a8129085b1eb8479cd2
AnuragVatash <109451690+AnuragVatash@users.noreply.github.com>
2025-08-01 08:46:16 -0700

    building casino fprint

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,8 @@
 import CasinoFingerprint from './components/games/casinoFingerprint/CasinoFingerprint';
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-        <p className="text-lg text-gray-600">Your Next.js app is ready!</p>
+    <div>
         <CasinoFingerprint />
       </div>
-    </div>
   );
 }
