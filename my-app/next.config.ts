b3b9d6dd15ca02e9730ffb370ecfd27b81399fc9
ManpreetS2 <manpreetsm2@gmail.com>
2025-09-16 22:05:33 -0700

    chore(dev): disable webpack FS cache in dev to avoid ENOENT rename errors

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { dev }) => {
+    // Prevent ENOENT rename errors from webpack's FS cache during dev on some systems
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
